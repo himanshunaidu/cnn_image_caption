8d9486879b512209bc0153ca9aae9a3e7877379d
Himanshu First <himanshunaidu12161997@gmail.com>
2021-07-20 00:21:56 +0530

    Bug Fixes

--- prep_ds.py
+++ prep_ds.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.preprocessing.text import Tokenizer
@@ -26,6 +27,11 @@ def data_prep(data_dict, tokenizer, max_length, vocab_size, base_path, ds_path):
     return np.array(X), np.array(y)
     return X, y
 
+#Utility Function for replacing last term (if required)
+def replace_last(source_string, replace_what, replace_with):
+    head, _sep, tail = source_string.rpartition(replace_what)
+    return head + replace_with + tail
+
 # Load the numpy files
 def map_func(img_name, cap):
     print(type(img_name))
@@ -34,7 +40,7 @@ def map_func(img_name, cap):
     img_tensor = np.load(img_name.replace(ds_path, feature_path, 1) + '.npy')
     return img_tensor, cap
 
-def lazy_load_ds():
+def lazy_load_ds(train_X, train_y, BUFFER_SIZE, BATCH_SIZE):
     dataset = tf.data.Dataset.from_tensor_slices((train_X, train_y))
 
     # Use map to load the numpy files in parallel
